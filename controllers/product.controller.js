51a3dd53b6a74fbf71773e3de470b97666e74389
EloraBarua <elorabarua62@gmail.com>
2023-04-13 23:00:01 +0600

    search and filter updated

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -27,125 +27,121 @@ exports.addProduct = async(req , res) => {
 // Get Product List
 exports.getProductList = async(req, res) => {
     try{
-        // Product Query List
-        const dressTypeList = [
-          "Shalware Kameez",
-          "Saree",
-          "Lahanga",
-          "Kurtis",
-          "Gown",
-          "Borka",
-          "Dupatta",
-          "others"
-        ];
+      // Product Query List
+      const dressTypeList = [
+        "Shalware Kameez",
+        "Saree",
+        "Lahanga",
+        "Kurtis",
+        "Gown",
+        "Borka",
+        "Dupatta",
+        "Pant",
+        "others",
+      ];
 
-        let productListQuery = req.query;
-        
-        let {
-          page = parseInt(productListQuery?.page) - 1 || 0,
-          limit = parseInt(productListQuery?.limit) || 0,
-          search = productListQuery?.search || "",
-          // sort = productListQuery?.sort || "createdAt",
-          minPrice = productListQuery?.minPrice || 0,
-          maxPrice = productListQuery?.maxPrice || Number.MAX_SAFE_INTEGER,
-          categoryId = productListQuery?.categoryId || "",
-          brandName = productListQuery?.brandName || "",
-          dressType = productListQuery?.dressType || "All",
-          material = productListQuery?.material || "",
-          color = productListQuery?.color || "",
-          ...others
-        } = productListQuery;
+      let productListQuery = req.query;
 
+      let {
+        page = parseInt(productListQuery?.page) - 1 || 0,
+        limit = parseInt(productListQuery?.limit) || 0,
+        search = productListQuery?.search || "",
+        // sort = productListQuery?.sort || "createdAt",
+        minPrice = productListQuery?.minPrice || 0,
+        maxPrice = productListQuery?.maxPrice || Number.MAX_SAFE_INTEGER,
+        categoryId = productListQuery?.categoryId || "",
+        brandName = productListQuery?.brandName || "",
+        dressType = productListQuery?.dressType || "All",
+        material = productListQuery?.material || "",
+        color = productListQuery?.color || "",
+        ...others
+      } = productListQuery;
 
-        console.log(page, limit,others,dressType)
-        // Array of DressType
-        dressType == "All" 
-        ? (dressType = [...dressTypeList]) 
+      // console.log(page, limit, others, dressType);
+      // Array of DressType
+      dressType == "All"
+        ? (dressType = [...dressTypeList])
         : (dressType = dressType.split(","));
 
-        // console.log(dressType)
-        // console.log(productListQuery);
+      // console.log(dressType)
+      // console.log(productListQuery);
 
-        // Array of BrandName
-        brandName = brandName.split(',');
-        
-        
-        // Array of material
-        material = material.split(',');
+      // Array of BrandName
+      brandName = brandName.split(",");
 
+      // Array of material
+      material = material.split(",");
 
-        // Array of color
-        color = color.split(',');
-        console.log(page, limit, brandName, dressType, material, color);
+      // Array of color
+      color = color.split(",");
+      // console.log(page, limit, brandName, dressType, material, color);
 
-        // const searchText = {$or : [{productName : {$regex: search, $options: "i"}} , {"brandId.brandName" : {$regex: search, $options: "i"}} , {"shopId.shopName" : {$regex: search, $options: "i"}}]};
-        
-        
-        const query = {
-          pricePerPiece: {
-            $gte: minPrice,
-            $lte: maxPrice,
-          },
-          // categoryId
-        };
+      // const searchText = {$or : [{productName : {$regex: search, $options: "i"}} , {"brandId.brandName" : {$regex: search, $options: "i"}} , {"shopId.shopName" : {$regex: search, $options: "i"}}]};
 
-        let searchText = {};
-        if(search != ''){searchText = {productName : {$regex: search, $options: "i"}};}
-        
+      const query = {
+        pricePerPiece: {
+          $gte: minPrice,
+          $lte: maxPrice,
+        },
+        // categoryId
+      };
 
-        productListQuery = Object.assign(searchText, others, query);
-        // console.log(productListQuery);
-        
-        // const page = parseInt(req.query.page)-1 || 0;
-        // const limit = parseInt(req.query.limit) || 10;
-        // const search = req.query.search || "";
-        // const sort = req.query.sort || "createdAt";
-        // const filterOptions = req.query.filterOptions || "All";
+      let searchText = {};
+      if (search != "") {
+        searchText = { productName: { $regex: search, $options: "i" } };
+      }
 
-        
-        
-        
-        // if (productListQuery?.minPrice || productListQuery?.maxPrice) {
-        //   // console.log(productListQuery);
-        //   // const { minPrice, maxPrice, ...others } = productListQuery;
-        //   // console.log(minPrice);
-        //   // console.log(maxPrice);
+      productListQuery = Object.assign(searchText, others, query);
+      // console.log(productListQuery);
 
-        //   const query = {
-        //     pricePerPiece: {
-        //       $gte: productListQuery?.minPrice || 0,
-        //       $lte: productListQuery?.maxPrice || Number.MAX_SAFE_INTEGER,
-        //     },
-        //   };
+      // const page = parseInt(req.query.page)-1 || 0;
+      // const limit = parseInt(req.query.limit) || 10;
+      // const search = req.query.search || "";
+      // const sort = req.query.sort || "createdAt";
+      // const filterOptions = req.query.filterOptions || "All";
 
-        //   productListQuery = Object.assign(others, query);
-        // }
+      // if (productListQuery?.minPrice || productListQuery?.maxPrice) {
+      //   // console.log(productListQuery);
+      //   // const { minPrice, maxPrice, ...others } = productListQuery;
+      //   // console.log(minPrice);
+      //   // console.log(maxPrice);
 
-        const productList = await Product.find(productListQuery)
-          .where("dressType")
-          .in([...dressType])
-          .where("material")
-          .in([...material])
-          .where("colorWithAvailableQuantity.color")
-          .in([...color])
-          .skip(page * limit);
-        
-        // const productList = await getProductListService(productListQuery);
+      //   const query = {
+      //     pricePerPiece: {
+      //       $gte: productListQuery?.minPrice || 0,
+      //       $lte: productListQuery?.maxPrice || Number.MAX_SAFE_INTEGER,
+      //     },
+      //   };
 
-        // console.log(productList)
-        if(!productList.length){
-            return res.status(204).json({
-            status: "fail",
-            message: "No Product available",
+      //   productListQuery = Object.assign(others, query);
+      // }
+
+      // const productList = await Product.find(productListQuery)
+      //   .where("dressType")
+      //   .in([...dressType])
+      //   .where("material")
+      //   .in([...material])
+      //   .where("colorWithAvailableQuantity.color")
+      //   .in([...color])
+      //   .skip(page * limit);
+
+      // const productList = await getProductListService(productListQuery);
+
+      // console.log(productList)
+
+      const productList = await Product.find(searchText).where("dressType").in([...dressType]);
+      if (!productList.length) {
+        return res.status(204).json({
+          status: "fail",
+          message: "No Product available",
         });
-        }
+      }
 
-          res.status(200).json({
-           status: "success",
-           message: "Product list loaded successfully",
-           data: productList,
-         });
-        
+      res.status(200).json({
+        status: "success",
+        message: "Product list loaded successfully",
+        data: productList,
+      });
     }
     catch(error){
         res.status(400).json({
